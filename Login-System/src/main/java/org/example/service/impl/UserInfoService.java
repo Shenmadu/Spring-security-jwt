56d4fea119964f16476bb5b3075c348b0886df63
shenmadu <tashen.madu@gmail.com>
2024-10-13 18:13:19 +0530

    first commit

---
+++ Login-System/src/main/java/org/example/service/impl/UserInfoService.java
@@ -0,0 +1,41 @@
+package org.example.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.UserDto;
+import org.example.entity.UserInfo;
+import org.example.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+
+public class UserInfoService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public String addUser(UserInfo userInfo) {
+        // Encode password before saving the user
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInfo> userDetail = repository.findByEmail(username); // Assuming 'email' is used as username
+
+        // Converting UserInfo to UserDetails
+        return userDetail.map(UserInfoDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
